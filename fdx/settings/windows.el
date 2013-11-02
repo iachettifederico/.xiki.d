b78a9fe1a92a5f78b2d88dfbf2343c141df96072
Federico Iachetti <iachetti.federico@gmail.com>
2013-11-02 16:37:24 -0300

    Initial commit

---
+++ fdx/settings/windows.el
@@ -0,0 +1,9 @@
+(windmove-default-keybindings)
+(winner-mode 1)
+
+;; Windmove for terminal
+(global-set-key "\M-[1;2A" 'windmove-up)
+(global-set-key (kbd "<select>") 'windmove-up)
+(global-set-key "\M-[1;2B" 'windmove-down)
+(global-set-key "\M-[1;2C" 'windmove-right)
+(global-set-key "\M-[1;2D" 'windmove-left)
---
+++ init.el
@@ -0,0 +1,26 @@
+;; FDX's Emacs configuration
+
+;;; TEMP ;;;
+(defun j2f ()
+  (interactive)
+  (save-excursion
+    (beginning-of-buffer)
+    (replace-regexp "jeg2" "fdx")))
+(global-set-key (kbd "<f6>") 'j2f)
+;;;;;;;;;;;;
+
+
+(defun fdx/load-init-file (path &optional noerror)
+  "This loads a file from inside the the .emacs.d directory"
+  (let ((file (file-name-sans-extension
+               (expand-file-name path user-emacs-directory))))
+    (load file noerror)))
+
+;; load configuration settings by type
+(fdx/load-init-file "fdx/init/freshen")
+(fdx/load-init-file "fdx/init/system")
+(fdx/load-init-file "fdx/init/autoloads")
+(fdx/load-init-file "fdx/init/packages")
+(fdx/load-init-file "fdx/init/compile")
+(fdx/load-init-file "fdx/init/autohooks")
+(fdx/load-init-file "fdx/init/settings")
