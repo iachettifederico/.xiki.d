e4ffb0ca97f89c46e1e30da95c2b08ac5f63d863
Federico Iachetti <iachetti.federico@gmail.com>
2013-11-03 21:16:39 -0300

    Configured ido-mode

--- fdx/settings/ido.el
+++ fdx/settings/ido.el
@@ -1,8 +1,25 @@
-(ido-mode 1)
-(setq ido-everywhere t
-      ido-auto-merge-work-directories-length -1
+;;;; Ido-mode
+;; Config M-x mode for ido
+(global-set-key
+ "\M-x"
+ (lambda ()
+   (interactive)
+   (call-interactively
+    (intern
+     (ido-completing-read
+      "M-x " (all-completions "" obarray 'commandp))))))
+
+(ido-mode t)
+
+;(ido-ubiquitous t)
+(setq ido-enable-prefix nil
       ido-enable-flex-matching t
-      ido-use-filename-at-point nil)
+      ido-auto-merge-work-directories-length nil
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess
+      ido-use-virtual-buffers t
+      ido-handle-duplicate-virtual-buffers 2
+      ido-max-prospects 10)
 
 ; Flex matching for ido-mode
 (require 'flx-ido)
