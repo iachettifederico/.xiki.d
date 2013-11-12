241c1e2d66cb45201c48567325f2d3cdbb0bda93
Federico Iachetti <iachetti.federico@gmail.com>
2013-11-12 12:06:46 -0300

    Some configs for rhtml-mode

---
+++ fdx/hooks/rhtml-mode.el
@@ -0,0 +1,5 @@
+(require 'expand-region)
+(local-set-key (kbd "M-r") 'er/expand-region)
+
+(local-set-key (kbd "C-=") 'fdx/reindent-buffer)
+(local-set-key (kbd "RET") 'fdx/indent-this-and-previous)
