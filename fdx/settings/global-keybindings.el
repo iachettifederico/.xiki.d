0679c7c672eae4033ec7d386e3941c4403f20717
Federico Iachetti <iachetti.federico@gmail.com>
2013-12-10 11:24:43 -0300

    Added custom global keybindings

---
+++ fdx/settings/global-keybindings.el
@@ -0,0 +1,8 @@
+(global-set-key (kbd "C-c s") 'query-replace-regexp)
+(global-set-key (kbd "C-c S") 'replace-regexp)
+
+(global-set-key (kbd "C-c i") 'align-regexp)
+
+
+(global-unset-key (kbd "M-l"))
+(global-set-key (kbd "M-l") 'kill-whole-line)
