15af5912799f19e920e2c8244bf486a253e096f8
Federico Iachetti <iachetti.federico@gmail.com>
2014-01-21 08:05:10 -0300

    Indentation hooks for some modes

--- fdx/hooks/ruby-mode.el
+++ fdx/hooks/ruby-mode.el
@@ -23,6 +23,7 @@
 ;; Delete trailing whitespace before saving
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+(local-unset-key (kbd "C-="))
 (local-set-key (kbd "C-=") 'fdx/reindent-buffer)
 (local-unset-key (kbd "M-="))
 (local-set-key (kbd "M-=") 'fdx/reindent-buffer)
@@ -32,7 +33,6 @@
 
 (require 'expand-region)
 (local-set-key (kbd "M-r") 'er/expand-region)
-
 
 ;;; Refactorings
 (local-set-key (kbd "C-c r l") 'fdx/refactoring/extract-let)
