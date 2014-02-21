56fb06d231454d43666333c79f7da35092096062
Federico Iachetti <iachetti.federico@gmail.com>
2014-02-21 13:32:52 -0300

    Added some cool commands for bufers and lines

--- fdx/settings/global-keybindings.el
+++ fdx/settings/global-keybindings.el
@@ -6,7 +6,13 @@
 
 (global-unset-key (kbd "M--"))
 (global-set-key (kbd "M--") 'kill-whole-line)
-
+(global-set-key (kbd "C-d") 'duplicate-line)
 
 (global-set-key (kbd "C-M-8") 'color-theme-ir-black)
 (global-set-key (kbd "C-M-9") 'color-theme-railscasts)
+
+(global-set-key (kbd "<M-down>") 'move-line-down)
+(global-set-key (kbd "<M-up>") 'move-line-up)
+
+(global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
+(global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
