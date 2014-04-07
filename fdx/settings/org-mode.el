dc064f090d133729985860072db5f6237151d21f
Federico Iachetti <iachetti.federico@gmail.com>
2014-04-07 16:02:28 -0300

    Org-babel + Ditaa working

--- fdx/settings/org-mode.el
+++ fdx/settings/org-mode.el
@@ -1,18 +1,18 @@
 (setq org-src-fontify-natively t)
 
+(setq org-ditaa-jar-path "/usr/bin/ditaa")
 
-(setq org-ditaa-jar-path (expand-file-name "fdx/vendor/ditaa0_9.jar" user-emacs-directory))
-(setq org-plantuml-jar-path (expand-file-name "fdx/vendor/plantuml.jar" user-emacs-directory))
+(setq org-plantuml-jar-path
+      (expand-file-name "fdx/vendor/plantuml.jar" user-emacs-directory))
 
-(add-hook 'org-babel-after-execute-hook 'bh/display-inline-images 'append)
-
-; Make babel results blocks lowercase
+                                        ; Make babel results blocks lowercase
 (setq org-babel-results-keyword "results")
 
-(defun bh/display-inline-images ()
-  (condition-case nil
-      (org-display-inline-images)
-    (error nil)))
+;; (defun bh/display-inline-images ()
+;;   (condition-case nil
+;;       (org-display-inline-images)
+;;     (error nil)))
+;; (add-hook 'org-babel-after-execute-hook 'bh/display-inline-images 'append)
 
 (org-babel-do-load-languages
  (quote org-babel-load-languages)
@@ -30,10 +30,9 @@
          (plantuml . t)
          (latex . t))))
 
-; Do not prompt to confirm evaluation
-; This may be dangerous - make sure you understand the consequences
-; of setting this -- see the docstring for details
+                                        ; Do not prompt to confirm evaluation
+                                        ; This may be dangerous - make sure you understand the consequences
+                                        ; of setting this -- see the docstring for details
 (setq org-confirm-babel-evaluate nil)
-
-; Use fundamental mode when editing plantuml blocks with C-c '
+                                        ; Use fundamental mode when editing plantuml blocks with C-c '
 (add-to-list 'org-src-lang-modes (quote ("plantuml" . fundamental)))
