9506fe27eaa6ff6087a5fed9c64f8d45adc313fd
Federico Iachetti <iachetti.federico@gmail.com>
2014-05-06 10:46:26 -0300

    Added rabl and .env to ruby extensions

--- fdx/settings/extensions.el
+++ fdx/settings/extensions.el
@@ -4,3 +4,5 @@
 (add-to-list 'auto-mode-alist '("Gemfile\\'"    . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Guardfile\\'"  . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rabl\\'"  . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.env\\'"  . ruby-mode))
