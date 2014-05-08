28b48ae651ca3b14d544738369a091ac2c5a2ab1
Federico Iachetti <iachetti.federico@gmail.com>
2014-05-08 08:42:30 -0300

    New symbol syntax correctly highlighted on new named params

--- fdx/hooks/ruby-mode.el
+++ fdx/hooks/ruby-mode.el
@@ -11,7 +11,7 @@
 ;; Colorize symbols correctly in ruby-mode
 (font-lock-add-keywords
  'ruby-mode
- '(("\\(\\b\\sw[_a-zA-Z0-9]*:\\)\\(?:\\s-\\|$\\)" (1 font-lock-constant-face))))
+ '(("\\(\\b\\sw[_a-zA-Z0-9]*:\\)\\(?:\\s-\\|,\\|$\\)" (1 font-lock-constant-face))))
 
 ;; show junk whitespace
 (whitespace-mode)
