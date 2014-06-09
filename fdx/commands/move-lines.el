40cf4472cf63f017e232dd377ac04ec25fc3cc77
Federico Iachetti <iachetti.federico@gmail.com>
2014-06-08 23:47:19 -0300

    Updated move line commands

--- fdx/commands/move-lines.el
+++ fdx/commands/move-lines.el
@@ -1,18 +1,35 @@
 ;;;###autoload
-(defun move-line-down ()
+(defun move-line-up ()
+  "Move up the current line."
   (interactive)
-  (let ((col (current-column)))
-    (save-excursion
-      (forward-line)
-      (transpose-lines 1))
-    (forward-line)
-    (move-to-column col)))
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode))
 
 ;;;###autoload
-(defun move-line-up ()
+(defun move-line-down ()
+  "Move down the current line."
   (interactive)
-  (let ((col (current-column)))
-    (save-excursion
-      (forward-line)
-      (transpose-lines -1))
-    (move-to-column col)))
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+;; ;;;###autoload
+;; (defun move-line-down ()
+;;   (interactive)
+;;   (let ((col (current-column)))
+;;     (save-excursion
+;;       (forward-line)
+;;       (transpose-lines 1))
+;;     (forward-line)
+;;     (move-to-column col)))
+
+;; ;;;###autoload
+;; (defun move-line-up ()
+;;   (interactive)
+;;   (let ((col (current-column)))
+;;     (save-excursion
+;;       (forward-line)
+;;       (transpose-lines -1))
+;;     (move-to-column col)))
