ccabd50f07894540301947c8ee186b57386deebf
Federico Iachetti <iachetti.federico@gmail.com>
2014-06-10 16:29:48 -0300

    Remapped RET in coffee-mode and haml-mode

---
+++ fdx/hooks/coffee-mode.el
@@ -0,0 +1,2 @@
+(local-unset-key (kbd "RET"))
+(global-set-key (kbd "RET") 'newline-and-indent)
--- fdx/hooks/haml-mode.el
+++ fdx/hooks/haml-mode.el
@@ -1,6 +1,9 @@
 (local-set-key (kbd "C-c e") 'erb-to-haml)
 (local-set-key (kbd "C-c C-e") 'erb-to-haml)
 
+(local-unset-key (kbd "RET"))
+(global-set-key (kbd "RET") 'newline-and-indent)
+
 ;;;; activate character pairing
 ;;(require 'smartparens)
 ;;(smartparens-mode)
