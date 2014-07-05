73f2bb02ad3c5e43f4f2e7c5150418419e5dfdcb
Federico Iachetti <iachetti.federico@gmail.com>
2014-07-05 00:48:09 -0300

    DONE!

--- init.el
+++ init.el
@@ -1,12 +1,23 @@
+; Load el4r, which loads Xiki
+(add-to-list 'load-path "/home/fedex/.rvm/gems/ruby-1.9.3-p547/gems/trogdoro-el4r-1.0.10/data/emacs/site-lisp/")
+(require 'el4r)
+(el4r-boot)
+
 ;; FDX's Emacs configuration
 
+(define-key key-translation-map (kbd "C-c C-c") (kbd "<C-return>"))
+
+(setq user-emacs-directory "/home/fedex/.xiki.d/")
+
 (require 'autoload)
+
 
 (defun fdx/load-init-file (path &optional noerror)
   "This loads a file from inside the the .emacs.d directory"
   (let ((file (file-name-sans-extension
                (expand-file-name path user-emacs-directory))))
     (load file noerror)))
+
 
 ;; load configuration settings by type
 (fdx/load-init-file "fdx/init/freshen")
