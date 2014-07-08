8c6477797d7d44b4a2d3eff8ad73d17081d71ffe
Federico Iachetti <iachetti.federico@gmail.com>
2014-07-08 12:05:34 -0300

    Changed multiple cursors keybindings

--- fdx/settings/multiple-cursors.el
+++ fdx/settings/multiple-cursors.el
@@ -2,13 +2,14 @@
 
 ;; Multiple cursors
 (global-set-key (kbd "C-M-<return>") 'mc/edit-lines)
-(global-set-key (kbd "C-c a") 'mc/edit-lines)
+(global-set-key (kbd "C-c C-a") 'mc/edit-lines)
+(global-set-key (kbd "C-c a") 'mc/mark-all-like-this)
 
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 
-(global-set-key (kbd "C-c <") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c >") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C->") 'mc/mark-next-like-this)
 
-(global-set-key (kbd "C-.") 'mc/mark-all-like-this)
-(global-set-key (kbd "C-c .") 'mc/mark-all-like-this)
+;; (global-set-key (kbd "C-.") 'mc/mark-all-like-this)
+;; (global-set-key (kbd "C-c .") 'mc/mark-all-like-this)
