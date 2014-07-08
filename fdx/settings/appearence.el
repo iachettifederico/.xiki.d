ec0849737c3a7285a53a0c04cdde4889f3564087
Federico Iachetti <iachetti.federico@gmail.com>
2014-07-08 12:03:12 -0300

    Back to ir-black theme

--- fdx/settings/appearence.el
+++ fdx/settings/appearence.el
@@ -5,19 +5,18 @@
   (require 'color-theme)
   (color-theme-initialize)
 
-  ;;(require 'color-theme-ir-black)
-  ;;(color-theme-ir-black)
+  (require 'color-theme-ir-black)
+  (color-theme-ir-black)
 
-  (require 'color-theme-sanityinc-tomorrow)
-  (color-theme-sanityinc-tomorrow-night)
-
+  ;;(require 'color-theme-sanityinc-tomorrow)
+  ;;(color-theme-sanityinc-tomorrow-night)
 
   (custom-set-faces
    '(default ((t (:family "Inconsolata"
-                          :foundry "unknown"
-                          :slant normal
-                          :weight normal
-                          :height 120 :width normal)))))
+			  :foundry "unknown"
+			  :slant normal
+			  :weight normal
+			  :height 120 :width normal)))))
 
   )
 
