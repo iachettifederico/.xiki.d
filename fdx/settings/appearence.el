01af6ca1452e1e8c06bb8b042636e09d8999b81f
Federico Iachetti <iachetti.federico@gmail.com>
2014-08-25 19:11:35 -0300

    Fixed appearence

--- fdx/settings/appearence.el
+++ fdx/settings/appearence.el
@@ -1,25 +1,20 @@
-(defun normal-appearence ()
-  "Normal appearence for Emacs."
+"Normal appearence for Emacs."
 
-  (interactive)
-  (require 'color-theme)
-  (color-theme-initialize)
+(require 'color-theme)
+(color-theme-initialize)
 
-  (require 'color-theme-ir-black)
-  (color-theme-ir-black)
 
-  ;;(require 'color-theme-sanityinc-tomorrow)
-  ;;(color-theme-sanityinc-tomorrow-night)
+;;(require 'color-theme-sanityinc-tomorrow)
+;;(color-theme-sanityinc-tomorrow-night)
 
-  (custom-set-faces
-   '(default ((t (:family "Inconsolata"
-                          :foundry "unknown"
-                          :slant normal
-                          :weight normal
-                          :height 120 :width normal)))))
-
-  )
-
-(normal-appearence)
+(custom-set-faces
+ '(default ((t (:family "Inconsolata"
+                        :foundry "unknown"
+                        :slant normal
+                        :weight normal
+                        :height 120 :width normal)))))
 
 (show-paren-mode t)
+
+(require 'color-theme-ir-black)
+(color-theme-ir-black)
