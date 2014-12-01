72f22d37915f3f051cf284d54ec80a6a393439fb
Federico Iachetti <iachetti.federico@gmail.com>
2014-12-01 12:50:16 -0300

    Don't show or remove empty spaces at the beginning or end of the file

--- fdx/settings/whitespace.el
+++ fdx/settings/whitespace.el
@@ -4,7 +4,8 @@
 ;; (setq         tab-stop-list    (number-sequence 4 120 4))
 
 ;; ;; setup junk whitespace types for when whitespace mode is active
-(setq whitespace-style '(face tabs trailing empty))
+(setq whitespace-style '(face tabs trailing))
 (setq whitespace-trailing-regexp
       "\\b\\(\\(\t\\| \\|\xA0\\|\x8A0\\|\x920\\|\xE20\\|\xF20\\)+\\)$")
+
 ;; "\\b\\(\\(   \\| \\|\240\\|ࢠ\\|ठ\\|ภ\\|༠\\)+\\)$"
