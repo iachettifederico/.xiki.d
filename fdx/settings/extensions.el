4af04db845e155f668c79b74ce712b51e02444b3
Federico Iachetti <iachetti.federico@gmail.com>
2014-12-01 12:45:40 -0300

    Added .ru as a ruby extension

--- fdx/settings/extensions.el
+++ fdx/settings/extensions.el
@@ -1,6 +1,7 @@
 ;; Ruby
 (add-to-list 'auto-mode-alist '("Rakefile\\'"         . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake\\'"          . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru\\'"            . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile\\'"          . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec\\'"       . ruby-mode))
 (add-to-list 'auto-mode-alist '("Guardfile\\'"        . ruby-mode))
