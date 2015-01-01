eacc826497045c40697340fbd9262eda7f738834
Federico Iachetti <iachetti.federico@gmail.com>
2015-01-01 13:33:17 -0300

    Added auto indent for scss

--- fdx/hooks/scss-mode.el
+++ fdx/hooks/scss-mode.el
@@ -1,1 +1,6 @@
 (setq scss-compile-at-save nil)
+
+(local-unset-key (kbd "C-="))
+(local-set-key (kbd "C-=") 'fdx/reindent-buffer)
+(local-unset-key (kbd "M-="))
+(local-set-key (kbd "M-=") 'fdx/reindent-buffer)
