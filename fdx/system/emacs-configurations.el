a8954da7403bc6ac0a96658c116c71c1a9897091
Federico Iachetti <iachetti.federico@gmail.com>
2015-01-15 16:46:25 -0300

    Changed appearence of ansi colors to be more eye friendly

--- fdx/system/emacs-configurations.el
+++ fdx/system/emacs-configurations.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["black" "red2" "green" "yellow3" "DodgerBlue1" "medium purple" "deep sky blue" "white"])
  '(fci-rule-color "gray3")
  '(fci-rule-width 4)
  '(rspec-auto-scroll nil)
