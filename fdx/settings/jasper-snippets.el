6b18ff49cf312ead22afcc0dd85f2bdd68b330b1
Federico Iachetti <iachetti.federico@gmail.com>
2015-04-20 13:06:10 -0300

    Added some snippets

---
+++ fdx/settings/jasper-snippets.el
@@ -0,0 +1,22 @@
+(require 'yasnippet)
+
+(defvar adhoc-snippet-last nil)
+
+(defun adhoc-snippet-create (begin end)
+  (interactive "r")
+  (let* ((text (concat
+                (buffer-substring-no-properties begin end)
+                ;; "$0" is required as an exit marker
+                "$0")))
+    (setq adhoc-snippet-last text)
+    ;; a lot of crazy things break if we don't turn this on
+    (yas-minor-mode 1)
+    (yas-expand-snippet text begin end)))
+
+(defun adhoc-snippet-insert ()
+  (interactive)
+  ;; a lot of crazy things break if we don't turn this on
+  (yas-minor-mode 1)
+  (yas-expand-snippet adhoc-snippet-last))
+
+(provide 'jasper-snippet)
