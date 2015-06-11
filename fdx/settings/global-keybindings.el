ef7890ba9840e96f4cf3f8edc65e6fc0e87f068e
Federico Iachetti <iachetti.federico@gmail.com>
2015-06-11 11:23:00 -0300

    Change keybindings for Scratch and @welcome

--- fdx/settings/global-keybindings.el
+++ fdx/settings/global-keybindings.el
@@ -59,8 +59,8 @@
 
 (global-set-key (kbd "C-c k") 'kill-other-buffers)
 
-(global-set-key (kbd "C-x C-w") (lambda () (interactive) (switch-to-buffer "@welcome/")))
-(global-set-key (kbd "C-c C-w") (lambda () (interactive) (switch-to-buffer "*scratch*")))
+(global-set-key (kbd "C-c C-w") (lambda () (interactive) (switch-to-buffer "@welcome/")))
+(global-set-key (kbd "C-x C-w") (lambda () (interactive) (switch-to-buffer "*scratch*")))
 
 (global-set-key (kbd "<f11>") 'fullscreen-mode-fullscreen-toggle)
 
