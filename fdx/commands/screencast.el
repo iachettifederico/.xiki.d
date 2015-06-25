93b666d18fab0b91ec6adeff2f56494cd82c8ef8
Federico Iachetti <iachetti.federico@gmail.com>
2015-06-25 11:12:49 -0300

    Added screencast command

---
+++ fdx/commands/screencast.el
@@ -0,0 +1,8 @@
+;;;###autoload
+(defun fdx/setup-screencast-frame ()
+  "Set up emacs for screencasting"
+  (interactive)
+  (set-frame-font
+   "Inconsolata:pixelsize=20:foundry=unknown:weight=normal:slant=normal:width=normal:spacing=100:scalable=true"
+   nil)
+  )
