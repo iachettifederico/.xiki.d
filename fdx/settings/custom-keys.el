54619724caf2269891642b8e02ebd8aa974db98d
Federico Iachetti <iachetti.federico@gmail.com>
2015-08-05 11:29:38 -0300

    Disabled xiki

--- fdx/settings/custom-keys.el
+++ fdx/settings/custom-keys.el
@@ -1,3 +1,3 @@
-(define-key key-translation-map (kbd "C-x C-x") (kbd "<C-return>"))
+;(define-key key-translation-map (kbd "C-x C-x") (kbd "<C-return>"))
 
 (define-key key-translation-map (kbd "¤") (kbd "~"))
--- init.el
+++ init.el
@@ -1,18 +1,16 @@
 ; Load el4r, which loads Xiki
-(add-to-list 'load-path "/home/fedex/.rvm/gems/ruby-1.9.3-p551/gems/trogdoro-el4r-1.0.10/data/emacs/site-lisp/")
-(require 'el4r)
-(el4r-boot)
+;; (add-to-list 'load-path "/home/fedex/.rvm/gems/ruby-1.9.3-p551/gems/trogdoro-el4r-1.0.10/data/emacs/site-lisp/")
+;; (require 'el4r)
+;; (el4r-boot)
 
 ;; FDX's Emacs configuration
 (require 'autoload)
-
 
 (defun fdx/load-init-file (path &optional noerror)
   "This loads a file from inside the the .emacs.d directory"
   (let ((file (file-name-sans-extension
                (expand-file-name path user-emacs-directory))))
     (load file noerror)))
-
 
 ;; load configuration settings by type
 (fdx/load-init-file "fdx/init/freshen")
